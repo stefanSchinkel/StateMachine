40493938128e0f73c8028668f46a149068e18cb7
Stefan Schinkel <mail@dreeg.org>
2016-06-10 20:34:54 +0200

    use mocha/chai for testing

--- StateMachineTest.js
+++ StateMachineTest.js
@@ -1,14 +1,29 @@
-var test = require('unit.js');
+var assert = require('chai').assert;
 var StateMachine = require('./StateMachine');
 
-var sm = new StateMachine.StateMachine();
+describe('StateMachine', function () {
+  "use strict";
+  var sm;
+  // fixture
+  beforeEach(function () {
+    sm = new StateMachine.StateMachine();
+    sm.addTransition(1, 'a', 1);
+    sm.addTransition(1, 'a', 1);
+    sm.addTransition(1, 'b', 2);
+    sm.addTransition(1, 'd', 3);
+    sm.addTransition(2, 'c', 3);
+  });
 
-sm.addTransition(1, 'a', 1);
-sm.addTransition(1, 'b', 2);
-sm.addTransition(1, 'd', 3);
-sm.addTransition(2, 'c', 3);
+  // test cases
+  it('should reject aaa', function () {
+    assert.equal(false, sm.accepts('aaa'));
+  });
+  it('should accept aaa', function () {
+    assert.equal(true, sm.accepts('aaabc'));
+  });
+  it('should reject aaa', function () {
+    assert.equal(true, sm.accepts('abc'));
+  });
 
-
-sm.printTransitionTable();
-test.assert.equal(true, sm.accepts('aaa'));
-test.assert.equal(false, sm.accepts('aaad'))
+});
+;
